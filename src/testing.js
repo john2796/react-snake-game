b9c24c99eadec07888bc31e8bc19092f2b4f84fc
john2796 <jbmiranda22796@gmail.com>
2018-12-27 23:02:33 -0800

    redoing everything

---
+++ src/testing.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+// const input =
+//   `testing
+//  whatever
+//  testin
+//  whates
+//  sdjaflsa
+//  asfjksd
+//  dkjfkdj
+//  dajsfljdas
+//  kdjkdfdkfj
+//  djfksdjfl
+//  dsfjk
+//  djaklfj
+//  asdjfl
+// jkfljklkljljadsf`
+
+// const data = input.split('\n')
+// console.log(data)
+// for (let i = 0; i < data.length; i++) {
+//   for (let y = i + 1; y < data.length; y++) {
+//     const data1 = [...data[i]]
+//     const data2 = [...data[y]]
+//     let diff = data2.reduce((a, c, i) => a + (c === data1[i] ? 0 : 1)
+//       , 0)
+
+
+//     if (diff === 1) {
+//       console.log(data1)
+//       console.log(data2)
+//     }
+//   }
+// }
+function MySet() {
+  this.name = 'testing';
+}
+
+const unionSet = new MySet();
+console.log(unionSet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
